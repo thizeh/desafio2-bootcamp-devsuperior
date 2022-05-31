2b75da87d12ef33a4f3f688c40cb1a270ae0fdd1
thizeh <thiago02_08@hotmail.com>
2022-05-31 18:15:40 -0300

    findAll City test

---
+++ bds02-main/src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.bds02.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+	
+	public List<CityDTO> findAll(){
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+}
